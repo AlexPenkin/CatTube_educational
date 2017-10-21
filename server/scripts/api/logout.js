e7c629292209d7cc9164d00fbe46cee6d7849847
AlexPenkin <sanekpenkin@gmail.com>
2017-10-21 15:40:33 +0300

    cleaned logout server api

--- server/scripts/api/logout.js
+++ server/scripts/api/logout.js
@@ -1,13 +1,7 @@
-const send = require('koa-send');
-const passport = require('../bin/createServer').passport;
-
-const VIEW = '/public/dist/index.html';
-
 module.exports.default = (router) => {
     router.get('/logout', async (ctx) => {
         ctx.set('Cache-Control', 'public');
         ctx.status = 200;
         ctx.logout();
-        await send(ctx, VIEW);
     });
 };
